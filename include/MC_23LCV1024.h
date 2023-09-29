84b69826d8cb2969e635a9834d2ae0b16275344f
Ryan Sims <ryanjsims@arizona.edu>
2023-09-29 15:11:59 -0700

    Add 23LCV1024 control code

---
+++ include/MC_23LCV1024.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdint.h>
+#include <span>
+
+#define SPI_RX  16
+#define SPI_CS  17
+#define SPI_SCK 18
+#define SPI_TX  19
+
+class MC_23LCV1024 {
+public:
+    enum class mode: uint8_t {
+        byte = 0,
+        page = 0x80,
+        full = 0x40,
+        resv = 0xC0
+    };
+    MC_23LCV1024(uint32_t requested_baud);
+
+    int32_t read(uint32_t addr, std::span<uint8_t> dst);
+    int32_t write(uint32_t addr, std::span<uint8_t> src);
+
+    void set_mode(mode new_mode);
+    mode get_mode();
+
+    uint32_t baud() {
+        return m_baud;
+    }
+
+    static const uint32_t SIZE = 0x20000;
+private:
+    uint32_t m_baud;
+};
---
+++ src/MC_23LCV1024.cpp
@@ -0,0 +1,80 @@
+#include "MC_23LCV1024.h"
+
+#include <hardware/gpio.h>
+#include <hardware/spi.h>
+#include <pico/binary_info.h>
+
+#include <stdio.h>
+#include "logger.h"
+
+#define READ_OP  0x03
+#define WRITE_OP 0x02
+#define RDMR_OP  0x05
+#define WRMR_OP  0x01
+
+static inline void cs_select() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(SPI_CS, 0);  // Active low
+    asm volatile("nop \n nop \n nop");
+}
+
+static inline void cs_deselect() {
+    asm volatile("nop \n nop \n nop");
+    gpio_put(SPI_CS, 0);  // Active low
+    asm volatile("nop \n nop \n nop");
+}
+
+MC_23LCV1024::MC_23LCV1024(uint32_t requested_baud) {
+    m_baud = spi_init(spi0, 20000000);
+    gpio_set_function(SPI_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_TX, GPIO_FUNC_SPI);
+    gpio_set_function(SPI_RX, GPIO_FUNC_SPI);
+    bi_decl(bi_3pins_with_func(SPI_SCK, SPI_RX, SPI_TX, GPIO_FUNC_SPI));
+
+    gpio_init(SPI_CS);
+    gpio_set_dir(SPI_CS, GPIO_OUT);
+    gpio_put(SPI_CS, 1);
+    // Make the CS pin available to picotool
+    bi_decl(bi_1pin_with_name(SPI_CS, "SPI CS"));
+
+    info("Initialized spi baud rate at %d Hz\n", m_baud);
+}
+
+int32_t MC_23LCV1024::read(uint32_t addr, std::span<uint8_t> dst) {
+    if(addr >= SIZE) {
+        return -1;
+    }
+    uint8_t buf[4] = {READ_OP, (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, addr & 0xFF};
+    cs_select();
+    spi_write_blocking(spi0, buf, 4);
+    spi_read_blocking(spi0, 0x00, dst.data(), dst.size());
+    cs_deselect();
+}
+
+int32_t MC_23LCV1024::write(uint32_t addr, std::span<uint8_t> src) {
+    if(addr >= SIZE) {
+        return -1;
+    }
+    uint8_t buf[4] = {WRITE_OP, (addr >> 16) & 0xFF, (addr >> 8) & 0xFF, addr & 0xFF};
+    cs_select();
+    spi_write_blocking(spi0, buf, 4);
+    spi_write_blocking(spi0, src.data(), src.size());
+    cs_deselect();
+}
+
+void MC_23LCV1024::set_mode(MC_23LCV1024::mode new_mode) {
+    uint8_t buf[2] = {WRMR_OP, (uint8_t)new_mode};
+    cs_select();
+    spi_write_blocking(spi0, buf, 2);
+    cs_deselect();
+}
+
+MC_23LCV1024::mode MC_23LCV1024::get_mode() {
+    uint8_t src[2] = {WRMR_OP, 0x00};
+    uint8_t dst[2] = {WRMR_OP, 0x00};
+    cs_select();
+    spi_write_read_blocking(spi0, src, dst, 2);
+    cs_deselect();
+
+    return (MC_23LCV1024::mode)dst[1];
+}
