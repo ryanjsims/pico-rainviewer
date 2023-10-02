c385757e87725e239bbbd225a9df6bfc57f51449
Ryan Sims <ryanjsims@arizona.edu>
2023-10-02 15:18:33 -0700

    Add a coroutine based sram reading function

--- include/MC_23LCV1024.h
+++ include/MC_23LCV1024.h
@@ -2,11 +2,43 @@
 
 #include <stdint.h>
 #include <span>
+#include <coroutine>
 
 #define SPI_RX  16
 #define SPI_CS  17
 #define SPI_SCK 18
 #define SPI_TX  19
+
+#if defined(__cpp_impl_coroutine) && defined(__cpp_lib_coroutine)
+template <typename T>
+struct data_streamer {
+    struct promise_type;
+    using handle_type = std::coroutine_handle<promise_type>;
+
+    struct promise_type
+    {
+        T value;
+        data_streamer get_return_object();
+        std::suspend_always initial_suspend();
+        std::suspend_always final_suspend() noexcept;
+        void unhandled_exception();
+
+        template <std::convertible_to<T> From>
+        std::suspend_always yield_value(From&& from);
+        void return_void();
+    };
+
+    handle_type handle;
+    data_streamer(handle_type h);
+    ~data_streamer();
+    explicit operator bool();
+    T operator()();
+
+private:
+    bool full = false;
+    void fill();
+};
+#endif
 
 class MC_23LCV1024 {
 public:
@@ -22,6 +54,10 @@ public:
     int32_t write(uint32_t addr, std::span<uint8_t> src);
     void clear();
 
+#if defined(__cpp_impl_coroutine) && defined(__cpp_lib_coroutine)
+    data_streamer<uint8_t> stream(uint32_t addr, uint32_t size);
+#endif
+
     void set_mode(mode new_mode);
     mode get_mode();
 
@@ -33,3 +69,7 @@ public:
 private:
     static uint32_t m_baud;
 };
+
+#if defined(__cpp_impl_coroutine) && defined(__cpp_lib_coroutine)
+template class data_streamer<uint8_t>;
+#endif
--- src/MC_23LCV1024.cpp
+++ src/MC_23LCV1024.cpp
@@ -2,6 +2,7 @@
 
 #include <hardware/gpio.h>
 #include <hardware/spi.h>
+#include <hardware/sync.h>
 #include <pico/binary_info.h>
 
 #include <stdio.h>
@@ -47,22 +48,41 @@ int32_t MC_23LCV1024::read(uint32_t addr, std::span<uint8_t> dst) {
         return -1;
     }
     uint8_t buf[4] = {READ_OP, (uint8_t)((addr >> 16) & 0xFF), (uint8_t)((addr >> 8) & 0xFF), (uint8_t)(addr & 0xFF)};
+    uint32_t state = save_and_disable_interrupts();
     cs_select();
     spi_write_blocking(spi0, buf, 4);
     int32_t bytes_read = spi_read_blocking(spi0, 0x00, dst.data(), dst.size());
     cs_deselect();
+    restore_interrupts(state);
     return bytes_read;
 }
+
+#if defined(__cpp_impl_coroutine) && defined(__cpp_lib_coroutine)
+data_streamer<uint8_t> MC_23LCV1024::stream(uint32_t addr, uint32_t size) {
+    uint8_t data;
+    uint32_t count = 0;
+    uint8_t buf[4] = {READ_OP, (uint8_t)((addr >> 16) & 0xFF), (uint8_t)((addr >> 8) & 0xFF), (uint8_t)(addr & 0xFF)};
+    cs_select();
+    spi_write_blocking(spi0, buf, 4);
+    while(count < size) {
+        count += spi_read_blocking(spi0, 0x00, &data, sizeof(data));
+        co_yield data;
+    }
+    cs_deselect();
+}
+#endif
 
 int32_t MC_23LCV1024::write(uint32_t addr, std::span<uint8_t> src) {
     if(addr >= SIZE) {
         return -1;
     }
     uint8_t buf[4] = {WRITE_OP, (uint8_t)((addr >> 16) & 0xFF), (uint8_t)((addr >> 8) & 0xFF), (uint8_t)(addr & 0xFF)};
+    uint32_t state = save_and_disable_interrupts();
     cs_select();
     spi_write_blocking(spi0, buf, 4);
     int32_t bytes_written = spi_write_blocking(spi0, src.data(), src.size());
     cs_deselect();
+    restore_interrupts(state);
     return bytes_written;
 }
 
@@ -70,6 +90,7 @@ void MC_23LCV1024::clear() {
     info1("Clearing SPI SRAM...\n");
     uint8_t buf[4] = {WRITE_OP, 0x00, 0x00, 0x00};
     uint8_t clearbuf[256];
+    uint32_t state = save_and_disable_interrupts();
     cs_select();
     spi_write_blocking(spi0, buf, 4);
     int32_t bytes_written = 0;
@@ -77,6 +98,7 @@ void MC_23LCV1024::clear() {
         bytes_written += spi_read_blocking(spi0, 0, clearbuf, sizeof(clearbuf));
     }
     cs_deselect();
+    restore_interrupts(state);
     info1("Cleared SPI SRAM.\n");
 }
 
@@ -96,3 +118,63 @@ MC_23LCV1024::mode MC_23LCV1024::get_mode() {
 
     return (MC_23LCV1024::mode)dst[1];
 }
+
+uint32_t MC_23LCV1024::m_baud = 0;
+
+
+template <typename T>
+data_streamer<T>::data_streamer(handle_type h): handle(h) {}
+
+template <typename T>
+data_streamer<T>::~data_streamer() {
+    handle.destroy();
+}
+
+template <typename T>
+data_streamer<T>::operator bool() {
+    fill();
+    return !handle.done();
+}
+
+template <typename T>
+T data_streamer<T>::operator()() {
+    fill();
+    full = false;
+    return std::move(handle.promise().value);
+}
+
+template <typename T>
+void data_streamer<T>::fill() {
+    if (!full) {
+        handle();
+        full = true;
+    }
+}
+
+template <typename T>
+data_streamer<T> data_streamer<T>::promise_type::get_return_object() {
+    return data_streamer(handle_type::from_promise(*this));
+}
+
+template <typename T>
+std::suspend_always data_streamer<T>::promise_type::initial_suspend() {
+    return {};
+}
+
+template <typename T>
+std::suspend_always data_streamer<T>::promise_type::final_suspend() noexcept {
+    return {};
+}
+
+template <typename T>
+void data_streamer<T>::promise_type::unhandled_exception() {}
+
+template <typename T>
+template <std::convertible_to<T> From>
+std::suspend_always data_streamer<T>::promise_type::yield_value(From&& from) {
+    value = std::forward<From>(from);
+    return {};
+}
+
+template <typename T>
+void data_streamer<T>::promise_type::return_void() {}
